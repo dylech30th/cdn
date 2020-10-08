4ab6f8166218da7e46c45af28982e13bd56ab3f1
Rinacm <2653221698@qq.com>
2020-10-08 11:13:30 +0800

    fix js file

--- js/botui.js
+++ js/botui.js
@@ -18,6 +18,28 @@ for(var s=a.css,u=a.type,l=a.enterClass,f=a.enterToClass,p=a.enterActiveClass,d=
 
 !function(e,t){"use strict";"function"==typeof define&&define.amd?define([],function(){return e.BotUI=t(e)}):e.BotUI=t(e)}("undefined"!=typeof window?window:this,function(e,t){"use strict";return function(t,n){function o(e,t,n,o){return"<a class='botui-message-content-link' target='"+(o?"blank":"")+"' href='"+n+"'>"+t+"</a>"}function i(e){return e.replace(v.image,"<img class='botui-message-content-image' src='$2' alt='$1' />").replace(v.icon,"<i class='botui-icon botui-message-content-icon fa fa-$1'></i>").replace(v.link,o)}function r(e,t){var n=document.createElement("script");n.type="text/javascript",n.src=e,t&&(n.onload=t),document.body.appendChild(n)}function s(e){y.action.addMessage&&h.message.human({delay:100,content:e}),y.action.show=!y.action.autoHide}function a(e){if(!e.loading&&!e.content)throw Error('BotUI: "content" is required in a non-loading message object.');e.type=e.type||"text",e.visible=!e.delay&&!e.loading;var t=y.messages.push(e)-1;return new Promise(function(n,o){setTimeout(function(){e.delay&&(e.visible=!0,e.loading&&(e.loading=!1)),n(t)},e.delay||0)})}function u(e){return"string"==typeof e&&(e={content:e}),e||{}}function c(e,t){for(var n in e)t.hasOwnProperty(n)||(t[n]=e[n])}function d(e){if(!e.action)throw Error('BotUI: "action" property is required.')}function l(e){return d(e),c({type:"text",cssClass:"",autoHide:!0,addMessage:!0},e),y.action.type=e.type,y.action.cssClass=e.cssClass,y.action.autoHide=e.autoHide,y.action.addMessage=e.addMessage,new Promise(function(t,n){p=t,setTimeout(function(){y.action.show=!0},e.delay||0)})}if(n=n||{},!t)throw Error("BotUI: Container id is required as first argument.");if(!document.getElementById(t))throw Error("BotUI: Element with id #"+t+" does not exist.");if(!e.Vue&&!n.vue)throw Error("BotUI: Vue is required but not found.");var f,m,p,g={debug:!1,fontawesome:!0},h={},v={icon:/!\(([^\)]+)\)/gim,image:/!\[(.*?)\]\((.*?)\)/gim,link:/\[([^\[]+)\]\(([^\)]+)\)(\^?)/gim};e.Vue=e.Vue||n.vue;for(var b in g)n.hasOwnProperty(b)&&(g[b]=n[b]);e.Promise||Promise||options.promise||r("https://cdn.jsdelivr.net/es6-promise/4.1.0/es6-promise.min.js");var w={template:"<div class=\"botui botui-container\" v-botui-container><div class=\"botui-messages-container\"><div v-for=\"msg in messages\" class=\"botui-message\" :class=\"msg.cssClass\" v-botui-scroll><transition name=\"slide-fade\"><div v-if=\"msg.visible\" :class=\"[{human: msg.human, \'botui-message-content\': true}, msg.type]\"><span v-if=\"msg.type == \'text\'\" v-text=\"msg.content\" v-botui-markdown></span> <iframe v-if=\"msg.type == \'embed\'\" :src=\"msg.content\" frameborder=\"0\" allowfullscreen></iframe></div></transition><div v-if=\"msg.loading\" class=\"botui-message-content loading\"><i class=\"dot\"></i><i class=\"dot\"></i><i class=\"dot\"></i></div></div></div><div class=\"botui-actions-container\"><transition name=\"slide-fade\"><div v-if=\"action.show\" v-botui-scroll><form v-if=\"action.type == \'text\'\" class=\"botui-actions-text\" @submit.prevent=\"handle_action_text()\" :class=\"action.cssClass\"><i v-if=\"action.text.icon\" class=\"botui-icon botui-action-text-icon fa\" :class=\"\'fa-\' + action.text.icon\"></i> <input type=\"text\" ref=\"input\" :type=\"action.text.sub_type\" v-model=\"action.text.value\" class=\"botui-actions-text-input\" :placeholder=\"action.text.placeholder\" :size=\"action.text.size\" :value=\"action.text.value\" :class=\"action.text.cssClass\" required v-focus/> <button type=\"submit\" :class=\"{\'botui-actions-buttons-button\': !!action.text.button, \'botui-actions-text-submit\': !action.text.button}\"><i v-if=\"action.text.button && action.text.button.icon\" class=\"botui-icon botui-action-button-icon fa\" :class=\"\'fa-\' + action.text.button.icon\"></i> <span>{{(action.text.button && action.text.button.label) || \'Go\'}}</span></button></form><div v-if=\"action.type == \'button\'\" class=\"botui-actions-buttons\" :class=\"action.cssClass\"> <button type=\"button\" :class=\"button.cssClass\" class=\"botui-actions-buttons-button\" v-for=\"button in action.button.buttons\" @click=\"handle_action_button(button)\" autofocus><i v-if=\"button.icon\" class=\"botui-icon botui-action-button-icon fa\" :class=\"\'fa-\' + button.icon\"></i> {{button.text}}</button></div></div></transition></div></div>",data:function(){return{action:{text:{size:30,placeholder:"Write here .."},button:{},show:!1,type:"text",autoHide:!0,addMessage:!0},messages:[]}},computed:{isMobile:function(){return e.innerWidth&&e.innerWidth<=768}},methods:{handle_action_button:function(e){s(e.text);var t={type:"button",text:e.text,value:e.value};for(var n in e)e.hasOwnProperty(n)&&"type"!==n&&"text"!==n&&"value"!==n&&(t[n]=e[n]);p(t)},handle_action_text:function(){this.action.text.value&&(s(this.action.text.value),p({type:"text",value:this.action.text.value}),this.action.text.value="")}}};e.Vue.directive("botui-markdown",function(e,t){"false"!=t.value&&(e.innerHTML=i(e.textContent))}),e.Vue.directive("botui-scroll",{inserted:function(e){m.scrollTop=m.scrollHeight}}),e.Vue.directive("focus",{inserted:function(e){e.focus()}}),e.Vue.directive("botui-container",{inserted:function(e){m=e}}),f=new e.Vue({components:{"bot-ui":w}}).$mount("#"+t);var y=f.$children[0];return h.message={add:function(e){return a(u(e))},bot:function(e){return e=u(e),a(e)},human:function(e){return e=u(e),e.human=!0,a(e)},get:function(e){return Promise.resolve(y.messages[e])},remove:function(e){return y.messages.splice(e,1),Promise.resolve()},update:function(e,t){var n=y.messages[e];return n.content=t.content,n.visible=!t.loading,n.loading=!!t.loading,Promise.resolve(t.content)},removeAll:function(){return y.messages.splice(0,y.messages.length),Promise.resolve()}},h.action={show:l,hide:function(){return y.action.show=!1,Promise.resolve()},text:function(e){return d(e),y.action.text=e.action,l(e)},button:function(e){return d(e),e.type="button",y.action.button.buttons=e.action,l(e)}},g.fontawesome&&r("https://use.fontawesome.com/ea731dcb6f.js"),g.debug&&(h._botApp=f),h}});
 
+var options = [{
+    value: "ask_id",
+    text: "这个ID是什么来历呢?"
+}, {
+    value: "like",
+    text: "平时除了写代码喜欢干什么呢"
+}, {
+    value: "showoff",
+    text: "封面的标语是什么意思呢?"
+}]
+
+function removeOptions(tag) {
+    options = options.filter(o => o.value != tag)
+}
+
+function constructAction() {
+    return botui.message.add({
+        delay: 1500,
+        options
+    })
+}
+
 function bot_ui_ini() {
     var botui = new BotUI("self-intro-bot")
     botui.message.add({
@@ -99,77 +121,77 @@ function bot_ui_ini() {
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 200,
+                            delay: 1000,
                             content: "包含如下列表:"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "计算机网络：自顶向下方法"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "现代编译原理: C语言实现"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "TCP/IP详解(全三卷)"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "深入理解计算机系统(CS:APP)"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "计算机组成与设计：硬件/软件接口"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "计算机体系结构：量化研究方法"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "计算机程序的构造和解释(SICP)"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "高级编译器设计与实现"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "x86汇编语言"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "深入理解JVM"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "深入理解JVM字节码"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "深入理解Kotlin协程"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "Java并发编程"
                         })
                     }).then(function() {
                         return botui.message.add({
-                            delay: 300,
+                            delay: 1000,
                             content: "......"
                         })
                     }).then(function() {
@@ -256,10 +278,14 @@ function bot_ui_ini() {
                             }, {
                                 text: "平时除了写代码喜欢干什么呢",
                                 value: "like"
+                            }, {
+                                text: "主页面的标语是什么意思?",
+                                value: "showoff"
                             }]
                         })
                     }).then(function(res) {
                         if (res.value == "ask_id") {
+                            removeOptions("ask_id")
                             botui.message.add({
                                 delay: 1000,
                                 content: "其实并没有什么奇怪的来历"
@@ -293,8 +319,13 @@ function bot_ui_ini() {
                                     delay: 1000,
                                     content: "╮(╯▽╰)╭"
                                 })
+                            }).then(function() {
+                                if (options.length != 0) {
+                                    return constructAction()
+                                } else return
                             })
-                        } else if (value == 'like') {
+                        } else if (res.value == 'like') {
+                            removeOptions("like")
                             botui.message.add({
                                 delay: 1000,
                                 content: "除了写代码以外大概就是看书和看番还有推galgame"
@@ -398,7 +429,41 @@ function bot_ui_ini() {
                                     delay: 1000,
                                     content: "Welcome to my blog, and enjoy your reading, then."
                                 })
+                            }).then(function() {
+                                if (options.length != 0) {
+                                    return constructAction()
+                                } else return
                             })
+                        } else if (res.value == 'showoff') {
+                            removeOptions("showoff")
+                            botui.message.add({
+                                delay: 1000,
+                                content: "清风迎客, 软语伴茶"
+                            }).then(function() {
+                                return botui.message.add({
+                                    delay: 1000,
+                                    content: "这是Win 7/Vista的字体测试文本"
+                                }).then(function() {
+                                    return botui.message.add({
+                                        delay: 1000,
+                                        content: "因为觉得意境非常棒，所以就拿来用了"
+                                    })
+                                }).then(function() {
+                                    return botui.message.add({
+                                        delay: 1000,
+                                        content: "正好也是我非常希望能够达到的境界和生活状态"
+                                    })
+                                }).then(function() {
+                                    return botui.message.add({
+                                        delay: 1000,
+                                        content: "That's all("
+                                    })
+                                }).then(function() {
+                                    if (options.length != 0) {
+                                        return constructAction()
+                                    } else return
+                                })
+                            }) 
                         }
                     })
                 }
